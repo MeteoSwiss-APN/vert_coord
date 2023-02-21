df77a7d78814a9507c11d3c3cbae9dd45fad8d56
Stephanie Westerhuis <stephanie.westerhuis@meteoswiss.ch>
2023-02-21 20:03:11 +0100

    add zoom

--- compare_nominal_level_distributions.py
+++ compare_nominal_level_distributions.py
@@ -15,9 +15,9 @@ out_dir = "/scratch/e1000/meteoswiss/scratch/swester/vert_coord/figures/nominal_
 c1 = "orchid"
 c2 = "slategray"
 c3 = "goldenrod"
-locations = [
-    "sav",
-]
+locations = ["sav", "mtblanc"]
+lev = 60
+lev_zoom = 20
 
 # files
 f1 = "/store/mch/msopr/swester/vert_coord_files/compare_nominal_levels/dwd.nc"
@@ -40,7 +40,7 @@ print("=== loading points of interest")
 poi = get_poi(loc=locations, lats=lats, lons=lons)
 
 for hhl in [hhl_1, hhl_2, hhl_3]:
-    info_hhl(hhl, poi, lev=15)
+    info_hhl(hhl, poi, lev=lev_zoom)
 
 levels = np.arange(81, 0, -1)
 for location in locations:
@@ -50,41 +50,41 @@ for location in locations:
     # altitude vs N level
     fig, ax = plt.subplots(figsize=(4, 6))
     ax.plot(
-        levels,
-        hhl_1[:, p.ind],
+        levels[-lev:],
+        hhl_1[-lev:, p.ind],
         color=c1,
         marker="o",
         markersize=2,
         label="107: DWD",
     )
     ax.plot(
-        levels,
-        hhl_2[:, p.ind],
+        levels[-lev:],
+        hhl_2[-lev:, p.ind],
         color=c2,
         marker="o",
         markersize=2,
         label="108: COSMO_20m",
     )
     ax.plot(
-        levels,
-        hhl_3[:, p.ind],
+        levels[-lev:],
+        hhl_3[-lev:, p.ind],
         color=c3,
         marker="o",
         markersize=2,
-        label="109: 'COSMO'_15m",
+        label="109: COSMO_15m",
     )
     ax.set_xlabel("Level")
     ax.set_ylabel("Altitude [masl]")
-    ax.set_xlim(0, 55)
-    ax.set_ylim(0, 6000)
+    ax.set_xlim(0, lev)
+    ax.set_ylim(hhl_1[-1, p.ind], hhl_1[-lev, p.ind])
     ax.legend()
     ax.set_title("Altitude of model levels")
     out_name = Path(out_dir, f"altitude_levels_{location}")
     plt.savefig(out_name, dpi=200, bbox_inches="tight")
     print(f"Saved as: {out_name}.png")
 
-    ax.set_xlim(0, 20)
-    ax.set_ylim(0, 800)
+    ax.set_xlim(0, lev_zoom)
+    ax.set_ylim(hhl_1[-1, p.ind], hhl_1[-lev_zoom, p.ind])
     out_name = Path(out_dir, f"altitude_levels_{location}_zoom")
     plt.savefig(out_name, dpi=200, bbox_inches="tight")
     print(f"Saved as: {out_name}.png")
